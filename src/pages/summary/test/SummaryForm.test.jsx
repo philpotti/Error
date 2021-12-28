0ed31941124e0ed7ad512468721ea5fc4d8f786a
Malcolm Haslop <imhaslop@gmail.com>
2021-12-28 11:45:28 -0300

    PushingToSolveCourseIssue

---
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -0,0 +1,50 @@
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import SummaryForm from "../SummaryForm";
+import userEvent from "@testing-library/user-event";
+
+test("Initial conditions", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  expect(checkbox).not.toBeChecked();
+
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+  expect(confirmButton).toBeDisabled();
+});
+
+test("Checkbox enables button on first click and disables on second click", () => {
+  render(<SummaryForm />);
+  const checkbox = screen.getByRole("checkbox", {
+    name: /terms and conditions/i,
+  });
+  const confirmButton = screen.getByRole("button", { name: /confirm order/i });
+
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeEnabled();
+
+  userEvent.click(checkbox);
+  expect(confirmButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+  //popover starts out hidden
+  const nullPopover = screen.queryByText(/Salsa/i);
+  expect(nullPopover).not.toBeInTheDocument();
+
+  //popover appears upon mouseover of checkbox checkboxLabel
+  const termsAndConditions = screen.getByText(/terms and conditions/i);
+  userEvent.hover(termsAndConditions);
+
+  const popover = screen.getByText(/Salsa/i);
+  expect(popover).toBeInTheDocument();
+
+  //popover disappears when we mouse out
+  userEvent.unhover(termsAndConditions);
+  await waitForElementToBeRemoved(() => screen.queryByText(/Salsa/i));
+});
